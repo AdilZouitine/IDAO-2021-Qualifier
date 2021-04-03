56fe9005ebc915acd962a4b17fb057c86ef0ebd8
Adil Zouitine <adilzouitinegm@gmail.com>
2021-04-03 23:26:56 +0200

    final sub

--- src/adil_main.py
+++ src/adil_main.py
@@ -15,17 +15,32 @@ from loader.image_loader import IdaoDataset, IdaoInferenceDataset
 from loss import IdaoLoss, idao_metric
 from model.backbone_multihead import TwoHeadModel, IndependantTwoHeadModel
 from utils.logo import logo
+from utils.dummy_validation import dummy_validation
+from submission_maker import SubmissionMaker
 
 
-train_path: List[str] = glob("../data/track_1/idao_dataset/train/*/*.png")
+train_path: List[str] = glob("../data/track_1/idao_dataset/new_test/train/*/*.png")
+test_path: List[str] = glob("../data/track_1/idao_dataset/new_test/*_test/*.png")
+
+transform = transforms.Compose(
+    [
+        transforms.CenterCrop(128),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (-0.3918, -0.2711, -0.0477),
+            (0.0822, 0.0840, 0.0836),  # mean and std over all the dataset
+        ),
+    ]
+)
 
 fold_generator = logo(list_files=train_path)
-
+# fold_generator = dummy_validation(list_files=train_path)
 device = torch.device("cuda")
 
 for index, (fold_train_path, fold_val_path) in enumerate(fold_generator):
-    train_dataset = IdaoDataset(list_path=fold_train_path)
-    val_dataset = IdaoDataset(list_path=fold_val_path)
+
+    train_dataset = IdaoDataset(list_path=fold_train_path, transform=transform)
+    val_dataset = IdaoDataset(list_path=fold_val_path, transform=transform)
 
     train_dataloader = DataLoader(
         dataset=train_dataset, batch_size=50, shuffle=True, num_workers=8
@@ -37,16 +52,16 @@ for index, (fold_train_path, fold_val_path) in enumerate(fold_generator):
 
     resnet18 = models.resnet18(pretrained=True)
     backbone = nn.Sequential(nn.Sequential(*list(resnet18.children())[:-2]))
-    model = TwoHeadModel(backbone_model=backbone, embedding_size=512 * 7 * 7)
-    # model = IndependantTwoHeadModel(backbone_model=backbone, embedding_size=512 * 7 * 7)
+    # model = TwoHeadModel(backbone_model=backbone, embedding_size=512 * 7 * 7)
+    model = IndependantTwoHeadModel(backbone_model=backbone, embedding_size=512 * 7 * 7)
     model.to(device)
     optimizer = optim.Adam(params=model.parameters(), lr=5e-5)
     criterion = IdaoLoss(w_classification=1, w_regression=1)
-    metric = partial(idao_metric, do_round_kev=True)  # Callable
+    metric = partial(idao_metric, do_round_kev=True, do_update_class=True)  # Callable
     scheduler = None
 
     dict_loader = {"train": train_dataloader, "validation": val_dataloader}
-    num_epoch = 3
+    num_epoch = 2
 
     train(
         model=model,
@@ -58,3 +73,18 @@ for index, (fold_train_path, fold_val_path) in enumerate(fold_generator):
         scheduler=scheduler,
         device=device,
     )
+    model.eval()
+
+    test_dataset = IdaoInferenceDataset(list_path=test_path, transform=transform)
+
+    sub_maker = SubmissionMaker(
+        eval_model=model,
+        submission_template="../data/track1_predictions_example_2.csv",
+        test_dataset=test_dataset,
+        device="cuda",
+    )
+    print("Inference")
+    sub_maker.infer(
+        save_path=f"../result/dummy_lg_pred_da_new_2{index}.csv", rounding=True
+    )
+    torch.save(model.state_dict(), f"../result/model_{index}.pth")
--- src/model/backbone_multihead.py
+++ src/model/backbone_multihead.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class TwoHeadModel(nn.Module):
@@ -79,3 +80,67 @@ class IndependantTwoHeadModel(nn.Module):
         regression = self.regression_head(regression)
 
         return classif, regression
+
+
+class RegressionModel(nn.Module):
+    def __init__(self, backbone_model: nn.Module, embedding_size: int):
+        super().__init__()
+        self.backbone_model_regression = backbone_model
+        self.flatten = nn.Flatten()
+        self.final_pooling = nn.AdaptiveAvgPool2d(output_size=(7, 7))
+        self.activation = nn.LeakyReLU(negative_slope=0.05, inplace=True)
+        self.emb_linear_regression = nn.Linear(
+            in_features=embedding_size, out_features=256
+        )
+        self.regression_head = nn.Linear(in_features=256, out_features=6)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        regression = self.backbone_model_regression(x)
+        regression = self.final_pooling(regression)
+        regression = self.flatten(regression)
+        regression = self.activation(regression)
+        regression = self.emb_linear_regression(regression)
+        regression = self.activation(regression)
+        regression = self.regression_head(regression)
+        regression = F.softmax(regression, dim=1)
+        return regression
+
+
+def wrapper_regression(regression_value: torch.Tensor) -> torch.Tensor:
+    """
+    Convert energy to label.
+    Args:
+        regression_value (torch.Tensor): regression value
+    Returns:
+        torch.Tensor: output a class code
+    """
+    for index, energy in enumerate([1, 3, 6, 10, 20, 30]):
+        regression_value = torch.where(
+            regression_value == energy, index, regression_value
+        )
+
+    return regression_value
+
+
+def pred_to_regression(
+    regression_predicted: torch.Tensor, argmax: bool = True
+) -> torch.Tensor:
+    """
+    Convert predicted class to predicted energy.
+
+    Args:
+        regression_predicted (torch.Tensor): [description]
+
+    Returns:
+        torch.Tensor: output an energy
+    """
+    if argmax:
+        regression_predicted = torch.argmax(regression_predicted, dim=1)
+    for index, energy in zip(range(5, -1, -1), reversed([1, 3, 6, 10, 20, 30])):
+
+        regression_predicted = torch.where(
+            regression_predicted == index, energy, regression_predicted
+        )  # noqa: E501
+
+    return regression_predicted
